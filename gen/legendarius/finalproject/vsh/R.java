1738806c838ca0fabec41fe03021f86fe17803db
LegenDarius116 <sanagusd@bxscience.edu>
2016-05-14 18:40:21 -0400

    Add files via upload

--- gen/legendarius/finalproject/vsh/R.java
+++ gen/legendarius/finalproject/vsh/R.java
@@ -21,24 +21,49 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int camera=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int btn_daily=0x7f080000;
-        public static final int btn_main=0x7f080001;
-        public static final int map=0x7f080002;
+        public static final int action_settings=0x7f080012;
+        public static final int back=0x7f080009;
+        public static final int btnRefresh=0x7f080011;
+        public static final int challenge_1=0x7f080008;
+        public static final int challenge_2=0x7f08000c;
+        public static final int challenge_3=0x7f08000e;
+        public static final int checkBox1=0x7f08000a;
+        public static final int checkBox2=0x7f08000b;
+        public static final int checkBox3=0x7f08000d;
+        public static final int imageButton=0x7f080010;
+        public static final int imageButton1=0x7f080003;
+        public static final int instruction=0x7f080004;
+        public static final int mainChallenges=0x7f080006;
+        public static final int randomChallenge=0x7f080005;
+        public static final int text1=0x7f080001;
+        public static final int text2=0x7f080002;
+        public static final int textChallenge=0x7f08000f;
+        public static final int textView1=0x7f080007;
+        public static final int title=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_instructions=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_main_challenges=0x7f030002;
+        public static final int activity_random_challenge=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int instructions=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int main_challenges=0x7f070002;
+        public static final int random_challenge=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_instructions=0x7f050004;
+        public static final int title_activity_main_challenges=0x7f050003;
+        public static final int title_activity_random_challenge=0x7f050005;
     }
     public static final class style {
         /** 
@@ -67,5 +92,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int MainTheme=0x7f060002;
     }
 }
