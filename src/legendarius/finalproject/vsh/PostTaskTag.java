169f6f61a694b7c422f28b0560f76f5f13c20500
Darius <sanagusd@bxscience.edu>
2016-05-19 13:53:45 -0400

    Add files via upload

--- src/legendarius/finalproject/vsh/PostTaskTag.java
+++ src/legendarius/finalproject/vsh/PostTaskTag.java
@@ -1,17 +1,22 @@
 package legendarius.finalproject.vsh;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,11 +36,15 @@ public class PostTaskTag extends AsyncTask<String, String, String> {
 		// TODO Auto-generated method stub
 
 		try {
-			useURL = false;
+			// change this value in code to determine whether online pic will be used
+			// or offline pic (found in my personal smartphone) will be used
+			useURL = true;
 			
 	    	httpclient = new DefaultHttpClient();
-	    	HttpPost httppost = new HttpPost("https://api.clarifai.com/v1/tag/");
-	    	httppost.addHeader("Authorization: Bearer", Credentials.accessToken);
+	    	HttpPost httppost = new HttpPost(new URI("https://api.clarifai.com/v1/tag/"));
+	    	httppost.addHeader(BasicScheme.authenticate(
+	    			new UsernamePasswordCredentials("Authorization: Bearer", Credentials.accessToken),
+	    			HTTP.UTF_8, false));
 	    	
 	    	List<NameValuePair> nvp = new ArrayList<NameValuePair>(1);
 	    	
@@ -54,7 +63,10 @@ public class PostTaskTag extends AsyncTask<String, String, String> {
 	    	Log.i("TagTest", resp); // Token invalid, authentication error returned
 	    	
 	    	JSONObject jo = new JSONObject(resp);
-		} catch (ClientProtocolException e) {} catch (IOException e) {} catch (JSONException e) {}
+		} catch (ClientProtocolException e) {} catch (IOException e) {} catch (JSONException e) {} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
